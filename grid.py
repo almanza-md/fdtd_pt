3be68cbb875faeafbc6f9e2e9a1a48afb7173591
Mark Almanza <almanzam@ucla.edu>
2022-05-09 13:13:27 -0700

    fixing cuda issue

--- grid.py
+++ grid.py
@@ -7,14 +7,14 @@ torch.set_default_dtype(torch.float32)
 def grid_setup(ndelta, res, L=torch.tensor(2)):
     device = ndelta.device
     nx = 2 * L * res + 1  # torch.floor(2*L*res).to(torch.int64)
-
+    L = L.to(device)
     x = torch.linspace(-L, L, nx.item())
 
     dx = x[1] - x[0]
     pmlx = torch.stack([L + n * dx for n in torch.arange(1, ndelta.item() + 1)])
 
     x = torch.cat((-1 * torch.flipud(pmlx), x, pmlx))
-    x.to(device=device)
+    x = x.to(device=device)
     dx = x[1] - x[0]
     nx = x.shape[0]
     ny = nx

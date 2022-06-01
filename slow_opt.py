b87ff8636fd2d37a15b7737562440be859e9c938
Mark Almanza <almanzam@ucla.edu>
2022-06-01 16:26:46 -0700

    script takes command line args

--- slow_opt.py
+++ slow_opt.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+import sys
+import argparse
 #import matplotlib as mpl
 #import matplotlib.pyplot as plt
 
@@ -11,17 +13,29 @@ sys.path.insert(0, os.path.abspath('../'))
 from fdtd_pt.opt import auto_opt
 from tqdm import trange
 
+parser = argparse.ArgumentParser(description='Optimize current absorption profile')
+parser.add_argument('--cells', type=int, default=8)
+parser.add_argument('--learnse', action='store_false')
+parser.add_argument('--learnsb', action='store_true')
+parser.add_argument('--speed', type=float,default=0.3)
+parser.add_argument('--smooth',action='store_true')
+parser.add_argument('--filtw', type=int,default=4)
+parser.add_argument('--res', type=int,default=32)
+parser.add_argument('--x0', type=float,default=-1.0)
+parser.add_argument('--niter', type=int,default=10000)
+parser.add_argument('--lr', type=float,default=0.01)
+args = parser.parse_args(sys.argv)
 
-n=8
+n=args.cells
 init = (0.,10.,10.)
 theta = 0
-v = 0.3
+v = args.speed
 vx = v*cos(theta)
 vy = v*sin(theta)
-smooth=True
-filter_n=4
+smooth=args.smooth
+filter_n=args.filtw
 
-best,hist,Bf,Ef,big_L = auto_opt(32,n,vx=vx,vy=vy,x0=-1.0,n_iter=1000,init=init,learn_se=True,learn_sb=False,lr=0.001,smooth_current=smooth,filter_n=filter_n)
+best,hist,Bf,Ef,big_L = auto_opt(args.res,n,vx=vx,vy=vy,x0=args.x0,n_iter=args.niter,init=init,learn_se=args.learnse,learn_sb=args.learnse,lr=args.lr,smooth_current=smooth,filter_n=filter_n)
 torch.save(best['alpha'],f'/mnt/PAULO/mark/pml/2D/alpha_profile_slow_{n}.pyt')
 torch.save(best['sigma'],f'/mnt/PAULO/mark/pml/2D/sigma_0_slow_{n}.pyt')
 torch.save(best['sigmastar'],f'/mnt/PAULO/mark/pml/2D/sigmastar_0_slow_{n}.pyt')
